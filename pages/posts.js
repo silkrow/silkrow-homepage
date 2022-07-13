13009d61557e238ffab1b2682c5a1137839c029e
silkrow <si1krow@outlook.com>
2022-07-13 17:23:00 +0800

    try to add post

--- pages/posts.js
+++ pages/posts.js
@@ -1,12 +1,29 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+
+import thumbMIT6S081notes from '../public/images/contents/MIT6S081notes.png'
+
 const Posts = () => (
   <Layout title="Posts">
     <Container>
       <Heading as="h3" fontSize={20} mb={4}>
-        Popular Posts
+        Posts
       </Heading>
-      Coming soon...
+
+      <Section delay={0.1}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="My notes for course MIT 6.S081"
+            thumbnail={thumbMIT6S081notes}
+            href="https://silkrow.notion.site/MIT-6-S081-4bd01c6d1cda4e9abdfdb669f1f4cc52"
+          />
+        </SimpleGrid>
+      </Section>
+
+
+
     </Container>
   </Layout>
 )
