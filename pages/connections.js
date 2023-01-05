cdacd1bebd80bce0a75e2398873c0dfe78b4bd8a
silkrow <si1krow@outlook.com>
2023-01-04 18:01:07 -0600

    update pages

--- pages/connections.js
+++ pages/connections.js
@@ -1,6 +1,18 @@
 import { Container, Heading} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import NextLink from 'next/link'
+import Paragraph from '../components/paragraph'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
 
 const Connections = () => (
   <Layout title="Connections">
