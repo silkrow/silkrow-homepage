ff4630cd76dd2225c3dda5047da978e1fb5d72ce
silkrow <si1krow@outlook.com>
2023-01-04 18:52:14 -0600

    change theme color

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -17,7 +17,7 @@ const ThemeToggleButton = () => {
       >
         <IconButton
           aria-label="Toggle theme"
-          colorScheme={useColorModeValue('gray', 'orange')}
+          colorScheme={useColorModeValue('green', 'orange')}
           icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
           onClick={toggleColorMode}
         ></IconButton>
